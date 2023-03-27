c17d4060d7d8c457f747d431dcef1b81dd836587
laiozago <laiozago@gmail.com>
2023-03-27 19:02:32 -0300

    inicio do front 3

---
+++ front end 2/semana3/Atividade Prática - Bootstrap 02/1 - modal.js
@@ -0,0 +1,9 @@
+document.getElementById("btnSim").addEventListener("click", function(){
+    console.log('Exclusão dos dados confirmada');
+}
+);
+
+document.getElementById("btnNao").addEventListener("click", function(){
+    console.log('Exclusão dos dados NÃO confirmada');
+}
+);
